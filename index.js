9dbb2933181c6c29de039bad9b4dd4eca17ec16d
Darryl Johnson <dandycodes@gmail.com>
2022-08-17 14:59:27 -0400

    inquirer file changed prompt works test need to be written next 8/17

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
-
+const path = require('path')
 const fs = require('fs')
 const inquirer = require('inquirer')
+const fetch = require('node-fetch')
 
 const teamBuilder = [];
 
 
 // below contains section for the manager prompt
-
 const managerPrompt = () => {
-    return inquirer.prompt ([
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -68,10 +68,15 @@ const managerPrompt = () => {
             }
         }
     ])
-    .then (managerData => {
-        const {name, email, id, officeNumber} = managerData;
-        const manager = new Manager (name, email, id, officeNumber)
-    })
+        .then(managerData => {
+            const { name, email, id, officeNumber } = managerData;
+            const manager = new Manager(name, email, id, officeNumber)
+
+            teamBuilder.push(manager)
+            console.log(manager)
+        })
+
+
 }
 
 
@@ -81,11 +86,124 @@ const newTeammate = () => {
     Adding employees to the team
     =================
     `);
-    return inquirer.prompt ([
+    return inquirer.prompt([
         {
-
+            type: 'list',
+            name: 'role',
+            message: 'Please choose a role!',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter an employee name',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            mesage: "Enter the employee ID",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log('Please enter a valid emplpyee number!')
+                    return false
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Enter Employee's GitHub",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log("Enter a valid username")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'college',
+            message: 'What school does the intern attend?',
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true
+                } else {
+                    console.log('Please enter the school')
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAdditional',
+            message: 'Would you like to add another member?',
+            default: false
         }
-    ]
+    ])
+    //below holds data fro emplyee
+    .then(teamData => {
 
-    )
+        let { name, id, email, role, github, school, confirmAdditional } = teamData; 
+        let employee; 
+
+        if (role === "Engineer") {
+            employee = new Engineer (name, id, email, github);
+
+            console.log(employee);
+
+        } else if (role === "Intern") {
+            employee = new Intern (name, id, email, school);
+
+            console.log(employee);
+        }
+
+        teamBuilder.push(employee); 
+
+        if (confirmAddEmployee) {
+            return addEmployee(teamBuilder); 
+        } else {
+            return teamBuilder;
+        }
+    })
+
+};
+
+
+//function below will create an html file using 'fs'
+//if page not created message displays
+
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your team profile is now available! Check out the index page!")
+        }
+    })
 }
+
+
+managerPrompt()
+    .then(newTeammate)
+    .then(teamBuilder => {
+        return generateHTML(teamBuilder)
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+        
