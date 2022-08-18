1f252fdb134c6efab9386dbe14544bb4624e976b
Darryl Johnson <dandycodes@gmail.com>
2022-08-18 13:41:45 -0400

    all test written to fail in employee test.js 8/18

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -13,3 +13,35 @@ test('create an object', () => {
     expect(employee.email).toEqual(expect.any(String))
 
 })
+
+//test below finds name of object
+
+test('name of object', () => {
+    const Employee = new Employee('John', 91, 'johndoe@gmail.com')
+
+    expect(employee.getName()).toEqual(expect.any(String));
+
+})
+
+//test below finds id of object
+
+test('id of object', () => {
+    const Employee = new Employee('John', 91, 'johndoe@gmail.com')
+
+    expect(employee.getId()).toEqual(expect.any(Number))
+})
+
+//test below finds email of object 
+
+test('email of object', () => {
+    const Employee = new Employee('John', 91, 'johndoe@gmail.com')
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()))
+})
+
+//test below finds the rold of object
+test('role of employee', () => {
+    const Employee = new Employee('John', 91, 'johndoe@gmail.com')
+
+    expect(employee.getRole()).toEqual("Employee")
+})
