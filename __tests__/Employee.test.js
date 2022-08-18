f144cb5f248186d9eca7f4387349d8c0da1b36ca
Darryl Johnson <dandycodes@gmail.com>
2022-08-18 13:24:28 -0400

    Object test failure success 8/18

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,15 @@
 //Write Employee Test First
+
+//below is the employee constructor
+const Employee = require('../lib/Employee')
+
+//need to test for an employee object first
+
+test('create an object', () => {
+    const Employee = new Employee('John', 91, 'johndoe@gmail.com')
+
+    expect(employee.name).toEqual(expect.any(String))
+    expect(employee.id).toEqual(expect.any(Number))
+    expect(employee.email).toEqual(expect.any(String))
+
+})
